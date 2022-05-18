bb8f90823544dfd296017107dcec18b7dfc67d56
Deddy Romnan <romnandeddy@gmail.com>
2022-05-18 15:40:32 +0700

    fix: ui improvement

--- app/src/main/java/com/ecosense/android/featDiscoverCampaign/presentation/BrowseCampaignScreen.kt
+++ app/src/main/java/com/ecosense/android/featDiscoverCampaign/presentation/BrowseCampaignScreen.kt
@@ -1,19 +1,18 @@
 package com.ecosense.android.featDiscoverCampaign.presentation
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Campaign
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ecosense.android.featDiscoverCampaign.presentation.component.CampaignItem
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -32,46 +31,15 @@ fun BrowseCampaignScreen(
             }
 
             Row {
-                LazyColumn {
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
                     items(viewModel.campaignList.value.size) { i ->
-                        val campaign = viewModel.campaignList.value[i]
-                        Row(
-                            modifier = Modifier.clickable {
-                                Log.d("TAG malas", "BrowseCampaignScreen: clicked ${campaign.title}")
+                        CampaignItem(
+                            campaign = viewModel.campaignList.value[i],
+                            onClick = {
+                                // TODO: navigate to CampaignDetailScreen
+                                Log.d("TAG", "BrowseCampaignScreen: clicked $i")
                             }
-                        ) {
-                            Column(
-                                modifier = Modifier.background(MaterialTheme.colors.primary)
-                            ) {
-                                if (campaign.isNew) Text(
-                                    text = "New",
-                                    color = MaterialTheme.colors.onPrimary
-                                )
-                                if (campaign.isTrending) Text(
-                                    text = "Trending",
-                                    color = MaterialTheme.colors.onPrimary
-                                )
-                                Icon(
-                                    imageVector = Icons.Default.Campaign,
-                                    contentDescription = campaign.title,
-                                    tint = MaterialTheme.colors.onPrimary
-                                )
-                            }
-                            Column {
-                                Text(
-                                    text = campaign.endDate,
-                                    style = MaterialTheme.typography.caption
-                                )
-                                Text(
-                                    text = campaign.title,
-                                    fontWeight = FontWeight.Bold
-                                )
-
-                                Text(text = campaign.category)
-
-                                Text(text = campaign.participantsCount.toString())
-                            }
-                        }
+                        )
                     }
                 }
             }
