b399d60b27b74f3cd2134e3b0157079938a40218
Deddy Romnan <romnandeddy@gmail.com>
2022-05-20 18:00:32 +0700

    feat: implement better reset password screen state handling

--- app/src/main/java/com/ecosense/android/featAuth/presentation/resetPassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/ecosense/android/featAuth/presentation/resetPassword/ResetPasswordScreen.kt
@@ -3,15 +3,21 @@ package com.ecosense.android.featAuth.presentation.resetPassword
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ecosense.android.R
 import com.ecosense.android.core.presentation.AuthNavGraph
 import com.ecosense.android.core.presentation.theme.spacing
+import com.ecosense.android.core.presentation.util.UIEvent
+import com.ecosense.android.core.presentation.util.asString
 import com.ecosense.android.featAuth.presentation.component.EmailTextField
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 @Destination
@@ -22,6 +28,28 @@ fun ResetPasswordScreen(
 ) {
     val scaffoldState = rememberScaffoldState()
 
+    val focusManager = LocalFocusManager.current
+    val context = LocalContext.current
+
+    val state = viewModel.state.value
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UIEvent.ShowSnackbar -> {
+                    focusManager.clearFocus()
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.uiText.asString(context)
+                    )
+                }
+
+                is UIEvent.HideKeyboard -> {
+                    focusManager.clearFocus()
+                }
+            }
+        }
+    }
+
     Scaffold(
         scaffoldState = scaffoldState,
         modifier = Modifier.fillMaxSize()
@@ -31,19 +59,33 @@ fun ResetPasswordScreen(
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
 
+            Text(text = stringResource(R.string.reset_password_caption))
+
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
+
             EmailTextField(
-                value = viewModel.email.value,
+                value = state.email,
                 onValueChange = { viewModel.onEmailValueChange(it) }
             )
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
 
             Button(
+                enabled = !state.isLoading,
                 onClick = { viewModel.onSendInstructionClick() },
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = stringResource(R.string.send_instruction))
+                Text(
+                    text = stringResource(
+                        if (state.isLoading) R.string.sending_instruction
+                        else R.string.send_instruction
+                    )
+                )
             }
+
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
+
+            if (state.isSuccessful) Text(text = stringResource(R.string.sm_reset_password))
         }
     }
 }
