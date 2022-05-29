c582909a4c6be360f9b2f4c3e2c0756776570814
Deddy Romnan <romnandeddy@gmail.com>
2022-05-29 15:09:17 +0700

    ui: customize background color and icon appearance

--- app/src/main/java/com/ecosense/android/core/presentation/component/BottomBar.kt
+++ app/src/main/java/com/ecosense/android/core/presentation/component/BottomBar.kt
@@ -1,17 +1,18 @@
 package com.ecosense.android.core.presentation.component
 
 import androidx.annotation.StringRes
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Campaign
+import androidx.compose.material.icons.filled.Eco
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Campaign
 import androidx.compose.material.icons.outlined.Eco
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ecosense.android.NavGraphs
 import com.ecosense.android.R
@@ -31,17 +32,22 @@ fun BottomBar(
     val currentDestination: Destination = navController.appCurrentDestinationAsState().value
         ?: NavGraphs.root.startAppDestination
 
-    BottomNavigation {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.surface,
+        contentColor = MaterialTheme.colors.primary,
+        elevation = 16.dp,
+    ) {
         BottomBarDestination.values().forEach { destination ->
+            val isSelected = currentDestination == destination.direction
             BottomNavigationItem(
-                selected = currentDestination == destination.direction,
+                selected = isSelected,
                 onClick = {
                     navController.navigateTo(destination.direction) { launchSingleTop = true }
                 },
                 icon = {
                     Icon(
-                        imageVector = destination.icon,
-                        contentDescription = stringResource(destination.label)
+                        imageVector = if (isSelected) destination.selectedIcon else destination.icon,
+                        contentDescription = stringResource(destination.label),
                     )
                 },
                 label = { Text(stringResource(destination.label)) },
@@ -53,23 +59,27 @@ fun BottomBar(
 private enum class BottomBarDestination(
     val direction: DirectionDestinationSpec,
     val icon: ImageVector,
+    val selectedIcon: ImageVector,
     @StringRes val label: Int
 ) {
     DiscoverCampaign(
         direction = DiscoverCampaignScreenDestination,
         icon = Icons.Outlined.Campaign,
+        selectedIcon = Icons.Filled.Campaign,
         label = R.string.campaign
     ),
 
     DiseaseRecognition(
         direction = DiseaseRecognitionScreenDestination,
         icon = Icons.Outlined.Eco,
+        selectedIcon = Icons.Filled.Eco,
         label = R.string.plant
     ),
 
     Profile(
         direction = ProfileScreenDestination,
         icon = Icons.Outlined.AccountCircle,
+        selectedIcon = Icons.Filled.AccountCircle,
         label = R.string.profile
     )
 }
