abd561d6437094a7c632568b93599c49b458a148
Deddy Romnan <romnandeddy@gmail.com>
2022-05-29 15:08:21 +0700

    ui: add custom colors

--- app/src/main/java/com/ecosense/android/core/presentation/theme/Color.kt
+++ app/src/main/java/com/ecosense/android/core/presentation/theme/Color.kt
@@ -2,10 +2,73 @@ package com.ecosense.android.core.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+//val Purple200 = Color(0xFFBB86FC)
+//val Purple500 = Color(0xFF6200EE)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
 
+val Gray50 = Color(0xfff8f8f8)
+val Gray100 = Color(0xfff0f0f0)
+val Gray200 = Color(0xffe6e6e6)
+val Gray300 = Color(0xffd5d5d5)
+val Gray400 = Color(0xffb0b0b0)
+val Gray500 = Color(0xff909090)
+val Gray600 = Color(0xff686868)
+val Gray700 = Color(0xff555555)
+val Gray800 = Color(0xff373737)
+val Gray900 = Color(0xff171717)
 
-val LightGrey200 = Color(0xfff8f8f8)
+//val Green50 = Color(0xffe2f4ec)
+//val Green100 = Color(0xffb8e3d0)
+//val Green200 = Color(0xff88d1b2)
+//val Green300 = Color(0xff50bf94)
+//val Green400 = Color(0xff04b27e)
+//val Green500 = Color(0xff00a568)
+//val Green600 = Color(0xff00965d)
+//val Green700 = Color(0xff008450)
+//val Green800 = Color(0xff007344)
+//val Green900 = Color(0xff00542e)
+
+val Green50 = Color(0xffe8f7ed)
+val Green100 = Color(0xffc9ebd2)
+val Green200 = Color(0xffa6deb6)
+val Green300 = Color(0xff82d299)
+val Green400 = Color(0xff65c883)
+val Green500 = Color(0xff49be6d)
+val Green600 = Color(0xff41ae63)
+val Green700 = Color(0xff379b56)
+val Green800 = Color(0xff2f8a4b)
+val Green900 = Color(0xff206a37) // Analogous to Primary
+
+val Teal50 = Color(0xffe1f2f1)
+val Teal100 = Color(0xffb5dfda)
+val Teal200 = Color(0xff86ccc3)
+val Teal300 = Color(0xff59b7ab)
+val Teal400 = Color(0xff3ca799)
+val Teal500 = Color(0xff2b9788)
+val Teal600 = Color(0xff288a7b)
+val Teal700 = Color(0xff247a6b)
+val Teal800 = Color(0xff206a5c) // Primary
+val Teal900 = Color(0xff194e41)
+
+val Lime50 = Color(0xfff2f8e6)
+val Lime100 = Color(0xffdfecc0)
+val Lime200 = Color(0xffc9e097)
+val Lime300 = Color(0xffb3d46c)
+val Lime400 = Color(0xffa1cb48)
+val Lime500 = Color(0xff90c21d)
+val Lime600 = Color(0xff80b214) // Secondary
+val Lime700 = Color(0xff6a9e04)
+val Lime800 = Color(0xff558b00)
+val Lime900 = Color(0xff2c6a00)
+
+val Orange50 = Color(0xfffff8e1)
+val Orange100 = Color(0xffffedb4)
+val Orange200 = Color(0xffffe283)
+val Orange300 = Color(0xffffd850)
+val Orange400 = Color(0xffffcd29)
+val Orange500 = Color(0xffffc408) // Accent
+val Orange600 = Color(0xffffb600)
+val Orange700 = Color(0xffffa300)
+val Orange800 = Color(0xffff9200)
+val Orange900 = Color(0xffff7300) // Accent
