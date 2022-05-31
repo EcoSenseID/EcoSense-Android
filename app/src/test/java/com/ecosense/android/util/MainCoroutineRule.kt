a7fb5b55c14a898ef639c4b51a5340061616cfcf
Deddy Romnan <romnandeddy@gmail.com>
2022-05-31 18:38:43 +0700

    test: add MainCoroutineRule.kt

---
+++ app/src/test/java/com/ecosense/android/util/MainCoroutineRule.kt
@@ -0,0 +1,27 @@
+package com.ecosense.android.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    private val dispatcher: CoroutineDispatcher = TestCoroutineDispatcher()
+) : TestWatcher(), TestCoroutineScope by createTestCoroutineScope(
+    TestCoroutineDispatcher() + TestCoroutineExceptionHandler() + dispatcher
+) {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
