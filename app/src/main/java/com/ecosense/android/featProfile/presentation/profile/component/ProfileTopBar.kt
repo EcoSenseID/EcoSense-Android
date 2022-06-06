6e4a03739dc2da3fb02df20d84f50615bc551b2d
Deddy Romnan <romnandeddy@gmail.com>
2022-06-06 15:54:06 +0700

    fix: add top bar title

--- app/src/main/java/com/ecosense/android/featProfile/presentation/profile/component/ProfileTopBar.kt
+++ app/src/main/java/com/ecosense/android/featProfile/presentation/profile/component/ProfileTopBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.ecosense.android.R
 
@@ -28,12 +29,27 @@ fun ProfileTopBar(
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Spacer(modifier = Modifier.weight(1f))
-            IconButton(onClick = onExpandDropdownMenu) {
-                Icon(
-                    imageVector = Icons.Default.MoreVert,
-                    contentDescription = stringResource(R.string.cd_options_menu),
-                    tint = MaterialTheme.colors.onSurface
-                )
+
+            Text(
+                text = stringResource(R.string.my_profile),
+                style = MaterialTheme.typography.h6,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+            ) {
+                IconButton(onClick = onExpandDropdownMenu) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = stringResource(R.string.cd_options_menu),
+                        tint = MaterialTheme.colors.onSurface,
+                    )
+                }
             }
         }
 
