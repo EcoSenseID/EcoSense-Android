99492d388fda3d13a9a19db820e1a2fbd1f5c9b2
BlingBong <rivano.atk@gmail.com>
2022-09-15 13:10:38 +0700

    feat: added default value for Rewards.kt

--- app/src/main/java/com/ecosense/android/featReward/domain/model/Rewards.kt
+++ app/src/main/java/com/ecosense/android/featReward/domain/model/Rewards.kt
@@ -3,4 +3,11 @@ package com.ecosense.android.featReward.domain.model
 data class Rewards(
     val category: String,
     val categoryRewards: List<CategoryRewards>
-)
+) {
+    companion object {
+        val defaultValue = Rewards(
+            category = "",
+            categoryRewards = emptyList()
+        )
+    }
+}
