e79229e387dccc9c6e9d8726b620432a73b6199b
Deddy Romnan <romnandeddy@gmail.com>
2022-09-20 23:03:52 +0700

    fix: firebase initialization issue

--- app/src/main/java/com/ecosense/android/application/EcoSenseApp.kt
+++ app/src/main/java/com/ecosense/android/application/EcoSenseApp.kt
@@ -1,6 +1,7 @@
 package com.ecosense.android.application
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 import logcat.AndroidLogcatLogger
 import logcat.LogPriority
@@ -9,6 +10,9 @@ import logcat.LogPriority
 class EcoSenseApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        FirebaseApp.initializeApp(this)
+
         AndroidLogcatLogger.installOnDebuggableApp(
             application = this,
             minPriority = LogPriority.VERBOSE
