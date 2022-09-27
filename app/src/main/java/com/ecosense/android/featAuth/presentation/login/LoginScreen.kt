45ebf27b9bf58c9db07ed0ce3838b80c6c9c2fbe
Deddy Romnan <romnandeddy@gmail.com>
2022-09-27 21:37:37 +0700

    revamp login screen

--- app/src/main/java/com/ecosense/android/featAuth/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/ecosense/android/featAuth/presentation/login/LoginScreen.kt
@@ -1,9 +1,15 @@
 package com.ecosense.android.featAuth.presentation.login
 
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -13,34 +19,40 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.ecosense.android.R
 import com.ecosense.android.core.presentation.AuthNavGraph
-import com.ecosense.android.core.presentation.component.RoundedEndsButton
+import com.ecosense.android.core.presentation.component.GradientButton
 import com.ecosense.android.core.presentation.theme.spacing
 import com.ecosense.android.core.presentation.util.UIEvent
 import com.ecosense.android.core.presentation.util.asString
 import com.ecosense.android.destinations.RegistrationScreenDestination
 import com.ecosense.android.destinations.ResetPasswordScreenDestination
+import com.ecosense.android.featAuth.presentation.component.AuthTopBar
 import com.ecosense.android.featAuth.presentation.component.EmailTextField
+import com.ecosense.android.featAuth.presentation.component.OutlinedGradientButton
 import com.ecosense.android.featAuth.presentation.component.PasswordTextField
 import com.ecosense.android.featAuth.presentation.login.contract.GoogleSignInContract
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kotlinx.coroutines.flow.collectLatest
 
+@OptIn(ExperimentalUnitApi::class)
 @Composable
 @Destination
 @AuthNavGraph(start = true)
 fun LoginScreen(
-    navigator: DestinationsNavigator,
-    viewModel: LoginViewModel = hiltViewModel()
+    navigator: DestinationsNavigator, viewModel: LoginViewModel = hiltViewModel()
 ) {
     val googleSignInLauncher = rememberLauncherForActivityResult(
-        contract = GoogleSignInContract
-    ) { idToken -> viewModel.onGoogleSignInResult(idToken = idToken) }
+        contract = GoogleSignInContract,
+        onResult = { idToken -> viewModel.onGoogleSignInResult(idToken = idToken) },
+    )
 
     val scaffoldState = rememberScaffoldState()
 
@@ -68,29 +80,30 @@ fun LoginScreen(
 
     Scaffold(
         scaffoldState = scaffoldState,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     ) {
         Column(
             modifier = Modifier
-                .padding(MaterialTheme.spacing.large)
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .background(MaterialTheme.colors.surface)
+                .padding(horizontal = MaterialTheme.spacing.medium)
         ) {
-            Text(
-                text = stringResource(R.string.login_screen_title),
-                style = MaterialTheme.typography.h6,
-                fontWeight = FontWeight.SemiBold,
-            )
+            AuthTopBar(onClickCancel = { navigator.navigateUp() })
+
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.extraLarge))
 
             Text(
-                text = stringResource(R.string.login_screen_caption),
-                style = MaterialTheme.typography.body1,
+                text = stringResource(R.string.login_title),
+                fontSize = TextUnit(34f, TextUnitType.Sp),
+                fontWeight = FontWeight.ExtraBold,
             )
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.large))
 
             EmailTextField(
                 value = state.email,
-                onValueChange = { viewModel.onEmailValueChange(it) }
+                onValueChange = { viewModel.onEmailValueChange(it) },
             )
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
@@ -99,28 +112,28 @@ fun LoginScreen(
                 value = state.password,
                 isVisible = state.isPasswordVisible,
                 onValueChange = { viewModel.onPasswordValueChange(it) },
-                onToggleVisibility = { viewModel.onTogglePasswordVisibility() }
+                onToggleVisibility = { viewModel.onTogglePasswordVisibility() },
             )
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
 
             Row(
                 horizontalArrangement = Arrangement.End,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
             ) {
                 Text(
                     text = stringResource(R.string.forgot_password),
-                    color = MaterialTheme.colors.primaryVariant,
+                    color = MaterialTheme.colors.secondary,
                     fontWeight = FontWeight.SemiBold,
                     modifier = Modifier.clickable {
                         navigator.navigate(ResetPasswordScreenDestination)
-                    }
+                    },
                 )
             }
 
-            Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.large))
 
-            RoundedEndsButton(
+            GradientButton(
                 enabled = !state.isLoading,
                 onClick = { viewModel.onLoginWithEmailClick() },
                 modifier = Modifier.fillMaxWidth()
@@ -129,36 +142,27 @@ fun LoginScreen(
                     text = stringResource(
                         if (state.isLoadingEmailLogin) R.string.logging_in
                         else R.string.login
-                    )
+                    ),
+                    color = MaterialTheme.colors.onPrimary,
+                    fontWeight = FontWeight.Bold,
                 )
             }
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
 
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Divider(modifier = Modifier.weight(1f))
-
-                Text(
-                    text = stringResource(R.string.or).uppercase(),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium)
-                )
-
-                Divider(modifier = Modifier.weight(1f))
-            }
+            Text(
+                text = stringResource(R.string.or).uppercase(),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth(),
+            )
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
 
-            RoundedEndsButton(
+            OutlinedGradientButton(
                 enabled = !state.isLoading,
                 onClick = { googleSignInLauncher.launch(0) },
                 modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = MaterialTheme.colors.surface,
-                )
             ) {
 
                 AsyncImage(
@@ -174,24 +178,24 @@ fun LoginScreen(
                         if (state.isLoadingGoogleLogin) R.string.logging_in
                         else R.string.continue_with_google
                     ),
-                    color = MaterialTheme.colors.primary,
+                    color = MaterialTheme.colors.onSurface,
+                    fontWeight = FontWeight.Bold,
                 )
             }
 
             Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
 
             Row(
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxWidth()
+                horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()
             ) {
                 Text(text = stringResource(R.string.new_here).plus(" "))
-                Text(
-                    text = stringResource(R.string.create_an_account),
-                    color = MaterialTheme.colors.primaryVariant,
+                Text(text = stringResource(R.string.create_an_account),
+                    color = MaterialTheme.colors.secondary,
                     fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.clickable { navigator.navigate(RegistrationScreenDestination) }
-                )
+                    modifier = Modifier.clickable { navigator.navigate(RegistrationScreenDestination) })
             }
+
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.extraLarge))
         }
     }
 }
