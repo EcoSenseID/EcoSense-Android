b817ae7d23ba5caa9eb1944a8a82281ccf78e974
BlingBong <rivano.atk@gmail.com>
2022-10-11 14:51:53 +0700

    feat: coded my reward detail as separate screen

--- app/src/main/java/com/ecosense/android/featReward/presentation/detail/myrewarddetail/MyRewardDetailScreenState.kt
+++ app/src/main/java/com/ecosense/android/featReward/presentation/detail/myrewarddetail/MyRewardDetailScreenState.kt
@@ -1,4 +1,27 @@
 package com.ecosense.android.featReward.presentation.detail.myrewarddetail
 
-class MyRewardDetailScreenState {
+import com.ecosense.android.featReward.domain.model.MyRewardDetail
+
+data class MyRewardDetailScreenState(
+    val myRewardDetail: MyRewardDetail,
+    val isLoadingMyRewardDetail: Boolean,
+    val isLoadingUseReward: Boolean
+) {
+    companion object {
+        val defaultValue = MyRewardDetailScreenState(
+            myRewardDetail = MyRewardDetail(
+                termsCondition = emptyList(),
+                bannerUrl = "",
+                description = "",
+                validity = "",
+                title = "",
+                partner = "",
+                pointsNeeded = 0,
+                claimStatus = 0,
+                howToUse = emptyList()
+            ),
+            isLoadingMyRewardDetail = false,
+            isLoadingUseReward = false
+        )
+    }
 }
--- app/src/main/java/com/ecosense/android/featReward/presentation/detail/myrewarddetail/MyRewardDetailViewModel.kt
+++ app/src/main/java/com/ecosense/android/featReward/presentation/detail/myrewarddetail/MyRewardDetailViewModel.kt
@@ -1,4 +1,80 @@
 package com.ecosense.android.featReward.presentation.detail.myrewarddetail
 
-class MyRewardDetailViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ecosense.android.R
+import com.ecosense.android.core.presentation.util.UIEvent
+import com.ecosense.android.core.util.Resource
+import com.ecosense.android.core.util.UIText
+import com.ecosense.android.featReward.domain.repository.RewardRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyRewardDetailViewModel @Inject constructor(
+    private val rewardRepository: RewardRepository
+) : ViewModel() {
+    private val _state = mutableStateOf(MyRewardDetailScreenState.defaultValue)
+    val state: State<MyRewardDetailScreenState> = _state
+
+    private val _eventFlow = Channel<UIEvent>()
+    val eventFlow = _eventFlow.receiveAsFlow()
+
+    private var getMyRewardDetail: Job? = null
+    fun getMyRewardDetail(rewardId: Int) {
+        getMyRewardDetail?.cancel()
+        getMyRewardDetail = viewModelScope.launch {
+            rewardRepository.getMyRewardDetail(rewardId = rewardId).onEach { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(isLoadingMyRewardDetail = false)
+                        result.uiText?.let { _eventFlow.send(UIEvent.ShowSnackbar(it)) }
+                    }
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            myRewardDetail = result.data ?: state.value.myRewardDetail,
+                            isLoadingMyRewardDetail = true
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            myRewardDetail = result.data ?: state.value.myRewardDetail,
+                            isLoadingMyRewardDetail = false
+                        )
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
+
+    private var onUseRewardJob: Job? = null
+    fun onUseRewardJob(rewardId: Int) {
+        onUseRewardJob?.cancel()
+        onUseRewardJob = viewModelScope.launch {
+            rewardRepository.setUseReward(rewardId = rewardId).onEach { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(isLoadingUseReward = false)
+                        result.uiText?.let { _eventFlow.send(UIEvent.ShowSnackbar(it)) }
+                    }
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(isLoadingUseReward = true)
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(isLoadingUseReward = false)
+                        _eventFlow.send(UIEvent.ShowSnackbar(UIText.StringResource(R.string.join_campaign_success)))
+                        getMyRewardDetail(rewardId = rewardId)
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
 }
