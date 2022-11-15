097018cbef238b79d319473fe96e171ca51b08c5
BlingBong <rivano.atk@gmail.com>
2022-11-15 09:28:34 +0700

    feat: created util function to make campaign countdown

--- app/src/main/java/com/ecosense/android/featDiscoverCampaign/data/util/DateUtils.kt
+++ app/src/main/java/com/ecosense/android/featDiscoverCampaign/data/util/DateUtils.kt
@@ -10,6 +10,7 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.*
 import kotlin.math.ceil
+import kotlin.math.floor
 
 @SuppressLint("SimpleDateFormat")
 fun dateFormatter(date: String): String {
@@ -33,4 +34,31 @@ fun countDays(date: String) : String {
     val result = ceil(count / 24)
 
     return result.toInt().toString()
+}
+
+fun unixCountdown(date: String): String {
+    val currentTime = System.currentTimeMillis() / 1000
+
+    val start = currentTime.toDouble()
+    val end = date.toDouble()
+
+    // get total seconds between the times
+    var delta = end - start
+
+    // calculate (and subtract) whole days
+    val days = floor(delta / 86400)
+    delta -= days * 86400
+
+    // calculate (and subtract) whole hours
+    val hours = floor(delta / 3600) % 24
+    delta -= hours * 3600
+
+    // calculate (and subtract) whole minutes
+    val minutes = floor(delta / 60) % 60
+    delta -= minutes * 60
+
+    // what's left is seconds
+    val seconds = floor(delta % 60)  // in theory the modulus is not required
+
+    return "${days.toInt()} Days ${hours.toInt()} Hrs ${minutes.toInt()} Mins ${seconds.toInt()} Secs"
 }
